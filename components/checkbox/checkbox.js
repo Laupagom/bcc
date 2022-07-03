dacd1a97a5792939dd9c5633129b97f79bb984b2
LPAGO9Y <laura.parreno@torresburriel.com>
2022-07-03 13:04:48 +0200

    switch-create

--- components/checkbox/checkbox.js
+++ components/checkbox/checkbox.js
@@ -1,5 +1,9 @@
-var disabledCheck = document.getElementById("myCheck").disabled;
+// var disabledCheck = document.getElementById("myCheck").disabled;
 
-function disableCheck() {
-    document.getElementById("myCheck").disabled = true;
-}
+// function disableCheck() {
+//     document.getElementById("myCheck").disabled = true;
+// }
+
+   const disabledCheck = document.getElementById('disabled');
+   disabledCheck.disabled = true; 
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_checkbox_disabled2
