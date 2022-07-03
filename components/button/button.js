b2c2390dd68464fcf4ba7411d220899f8eeeef87
LPAGO9Y <laura.parreno@torresburriel.com>
2022-07-03 12:26:23 +0200

    checkbox-functionjs

--- components/button/button.js
+++
@@ -1,2 +0,0 @@
-const btncompra = document.getElementById('place_order');
-btncompra.disabled = true; 
---
+++ components/checkbox/checkbox.js
@@ -0,0 +1,5 @@
+var disabledCheck = document.getElementById("myCheck").disabled;
+
+function disableCheck() {
+    document.getElementById("myCheck").disabled = true;
+}
