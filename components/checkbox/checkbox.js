8e5235f3c26138bed3235c0a6073aeaa6e66657a
LPAGO9Y <laura.parreno@torresburriel.com>
2022-07-04 13:19:43 +0200

    checkbox

--- components/checkbox/checkbox.js
+++ components/checkbox/checkbox.js
@@ -7,3 +7,4 @@
    const disabledCheck = document.getElementById('disabled');
    disabledCheck.disabled = true; 
 // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_checkbox_disabled2
+
